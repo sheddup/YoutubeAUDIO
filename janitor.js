72546ef77c4ae006968307e59b1e4565c29a7c44
sheddup <5480335+sheddup@users.noreply.github.com>
2018-10-16 16:25:59 -0700

    Delete janitor.js

--- janitor.js
+++
@@ -1,7 +0,0 @@
-var overlay = document.getElementById("playerOverlay");
-if(overlay) {
-  overlay.remove();
-}
-browser.runtime.sendMessage({msg : "clean"});
-//https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/tabs/executeScript#Return_value
-undefined;
